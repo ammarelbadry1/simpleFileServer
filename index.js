8b7356ab21371a0b3f32789f5c8301afabebe3c7
Ammar El-Badry <ammarelbadry1@gmail.com>
2024-07-02 12:33:48 +0300

    init

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const express = require('express');
-const path = require('path'); // for resolving file paths
+const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000; // change port if needed
+const port = 3000;
 
-// Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// List available static files
-app.get('/:dir/files', (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/ls', (req, res) => {
+  fs.readdir('public', (err, files) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error reading files');
+    }
+    files = files.filter(file => file !== 'favicon.ico' && file !== 'index.html');
+    res.json(files);
+  });
+});
+
+app.get('/:dir', (req, res) => {
   const dir = req.params.dir;
   fs.readdir(`public/${dir}`, (err, files) => {
     if (err) {
       console.error(err);
       return res.status(500).send('Error reading files');
     }
-    res.json(files); // Send the list of filenames as JSON
+    res.json(files);
   });
 });
 
-// List available static files
-app.get('/files', (req, res) => {
-  fs.readdir('public', (err, files) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send('Error reading files');
-    }
-    res.json(files); // Send the list of filenames as JSON
-  });
-});
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-  console.log(`Start with => http://localhost:${port}/files`);
+  console.log(`Start with => http://localhost:${port}`);
 });
